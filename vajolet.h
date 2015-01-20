865a44c1d44407cb22f8fd7ca28435d427ea9d98
Marco Belli <elcabesa@inwind.it>
2015-01-20 20:27:14 +0100

    position debug

--- vajolet.h
+++ vajolet.h
@@ -33,7 +33,7 @@
 //---------------------------------------------
 
 #define PROGRAM_NAME	"Vajolet2"
-#define VERSION			"2.0.010"
+#define VERSION			"2.0.011"
 #define PRECALCULATED_BITSET
 #define HW_BITCOUNT
 
