dc441d75352262a4139a65b1ff8daeb79f28301e
Marco Belli <elcabesa@inwind.it>
2016-01-04 18:22:41 +0100

    prova abortita

--- vajolet.h
+++ vajolet.h
@@ -33,7 +33,7 @@
 //---------------------------------------------
 
 #define PROGRAM_NAME	"Vajolet2"
-#define VERSION			"2.1.2"
+#define VERSION			"2.2.2"
 #define PRECALCULATED_BITSET
 #define HW_BITCOUNT
 
