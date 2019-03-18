5b0a33c3221ada420ac7f7760a90c01bda87eb72
marco <elcabesa@inwind.it>
2019-03-18 08:23:31 +0100

    bump version to 2.8

--- version.h
+++ version.h
@@ -21,7 +21,7 @@
 #include <string>
 
 const std::string programName = "Vajolet2";
-const std::string version = "2.7.0";
-const std::string preRelease = "-PRE-61";
+const std::string version = "2.8.0";
+const std::string preRelease = "-PRE-0";
 
 #endif
