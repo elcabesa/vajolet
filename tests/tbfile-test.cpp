cfdb442f1e469d40a1e573159fca2dee25d57d29
marco <elcabesa@inwind.it>
2019-05-04 17:03:21 +0200

    unit test1

--- tests/tbfile-test.cpp
+++ tests/tbfile-test.cpp
@@ -1,5 +1,49 @@
 #include "gtest/gtest.h"
+#include "syzygy/tbfile.h"
 
-TEST(tbfile, test) {
-	EXPECT_EQ(true, true);
+#ifndef _WIN32
+	constexpr char SepChar = ':';
+#else
+	constexpr char SepChar = ';';
+#endif
+
+TEST(tbfile, exist) {
+	EXPECT_FALSE(TBFile::exist("ciao.txt"));
+	EXPECT_FALSE(TBFile::exist("open.txt"));
+	
+	TBFile::setPaths("data");
+	
+	EXPECT_FALSE(TBFile::exist("ciao.txt"));
+	EXPECT_TRUE(TBFile::exist("open.txt"));
+	
+}
+
+TEST(tbfile, setPaths) {
+		
+	TBFile::setPaths("data");
+	
+	EXPECT_FALSE(TBFile::exist("ciao.txt"));
+	EXPECT_TRUE(TBFile::exist("open.txt"));
+	
+	TBFile::setPaths("");
+	EXPECT_FALSE(TBFile::exist("ciao.txt"));
+	EXPECT_FALSE(TBFile::exist("open.txt"));
+	
+	std::string s = "data";
+	s += SepChar;
+	s += "data2";
+	TBFile::setPaths(s);
+	
+	EXPECT_FALSE(TBFile::exist("ciao.txt"));
+	EXPECT_TRUE(TBFile::exist("open.txt"));
+	
+	s = "data2";
+	s += SepChar;
+	s += "data";
+	TBFile::setPaths(s);
+	
+	EXPECT_FALSE(TBFile::exist("ciao.txt"));
+	EXPECT_TRUE(TBFile::exist("open.txt"));
+	
+	
 }
