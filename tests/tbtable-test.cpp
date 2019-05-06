d94f05977adfa5aa9d75110c07cc678de1d996dd
marco <elcabesa@inwind.it>
2019-05-06 19:18:38 +0200

    add tests

---
+++ tests/tbtable-test.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "syzygy/tbtable.h"
+
+
+TEST(tbtable, constructor1) {
+	TBTable tbt("KBNvKB");
+	
+	getKey()
+	getKey2()
+	getPieceCount()
+	getPawnCount(unsigned int x)
+	hasPawns()
+	hasUniquePieces()
+	
+	EXPECT_FALSE(false);
+}
+
