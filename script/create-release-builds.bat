c876aa17f5583879e975f6a1c31d28c5d21072d7
marco <elcabesa@inwind.it>
2019-10-30 20:08:35 +0100

    build script fix

--- script/create-release-builds.bat
+++ script/create-release-builds.bat
@@ -2,6 +2,7 @@ pushd ..
 mkdir release
 popd
 
+CALL clean.bat
 CALL create-nopopcnt-build-clang.bat
 copy ..\build\src\Vajolet.exe ..\release\Vajolet2_2.8_old.exe
 CALL clean.bat
@@ -10,6 +11,6 @@ CALL create-popcnt-build-clang.bat
 copy ..\build\src\Vajolet.exe ..\release\Vajolet2_2.8.exe
 CALL clean.bat
 
-CALL create-bmi-build-clang.bat
+CALL create-bmi2-build-clang.bat
 copy ..\build\src\Vajolet.exe ..\release\Vajolet2_2.8_bmi.exe
 CALL clean.bat
