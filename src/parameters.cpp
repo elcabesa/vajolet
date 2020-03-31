d89bd77f0c695eb0db7991c6f1eb60ef8511fa64
marco <elcabesa@inwind.it>
2020-03-31 11:19:34 +0200

    tune

--- src/parameters.cpp
+++ src/parameters.cpp
@@ -195,34 +195,34 @@ simdScore mobilityBonus[separationBitmap][32] = {
 };
 
 SearchParameters::SearchParameters():
-razorMargin(19469),
+razorMargin(20346),
 razorMarginDepth(0),
 razorMarginCut(0),
-razorDepth(67),
+razorDepth(64),
 razorReturn(false),
 
-staticNullMovePruningDepth(129),
-staticNullMovePruningValue(365),
-staticNullMovePruningImprovingBonus(2142),
+staticNullMovePruningDepth(127),
+staticNullMovePruningValue(374),
+staticNullMovePruningImprovingBonus(2020),
 
-nullMovePruningDepth(17),
-nullMovePruningReduction(53),
+nullMovePruningDepth(16),
+nullMovePruningReduction(47),
 nullMovePruningBonusDepth(0.25),
-nullMovePruningBonusThreshold(9393),
+nullMovePruningBonusThreshold(10143),
 nullMovePruningBonusAdditionalRed(16),
-nullMovePruningVerificationDepth(196),
+nullMovePruningVerificationDepth(184),
 
-probCutDepth(85),
-probCutDelta(8164),
-probCutDepthRed(47),
-iidDepthPv(79),
-iidDepthNonPv(132),
-iidStaticEvalBonus(9576),
-iidDepthRed(32),
+probCutDepth(78),
+probCutDelta(8241),
+probCutDepthRed(49),
+iidDepthPv(81),
+iidDepthNonPv(130),
+iidStaticEvalBonus(9893),
+iidDepthRed(31),
 iidDepthRedFactor(4),
 
-singularExpressionPVDepth(96),
-singularExpressionNonPVDepth(125),
-singularExpressionTtDepth(48)
+singularExpressionPVDepth(93),
+singularExpressionNonPVDepth(127),
+singularExpressionTtDepth(49)
 
 {}
