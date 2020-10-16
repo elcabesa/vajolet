b726c2f571deae00fd106b7345d298000df0edda
marco belli <elcabesa@inwind.it>
2020-10-16 10:55:59 +0200

    open eval range

--- src/position/eval.cpp
+++ src/position/eval.cpp
@@ -771,7 +771,7 @@ Score Position::eval(void) const
     
 	// removing std::abs(res[1]) < 20000 speed 38kn/s
 	// with std::abs(res[1]) < 20000 speed 56kn/s
-    if(!trace && std::abs(res[1]) < 20000 && _nnue && _nnue->loaded()) {
+    if(!trace && std::abs(res[1]) < 200000 && _nnue && _nnue->loaded()) {
         return _nnue->eval();
     }
         
