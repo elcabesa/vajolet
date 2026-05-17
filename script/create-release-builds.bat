c6c044b7534dc29057d27f68d808991d0ab8cb41
marco belli <elcabesa@inwind.it>
2026-05-17 09:05:10 +0200

    update release notes

--- script/create-release-builds.bat
+++ script/create-release-builds.bat
@@ -4,13 +4,13 @@ popd
 
 CALL clean.bat
 CALL create-nopopcnt-build-clang.bat
-copy ..\build\src\Vajolet.exe ..\release\Vajolet2_3.1_old.exe
+copy ..\build\src\Vajolet.exe ..\release\Vajolet2_3.2_old.exe
 CALL clean.bat
 
 CALL create-popcnt-build-clang.bat
-copy ..\build\src\Vajolet.exe ..\release\Vajolet2_3.1.exe
+copy ..\build\src\Vajolet.exe ..\release\Vajolet2_3.2.exe
 CALL clean.bat
 
 CALL create-bmi2-build-clang.bat
-copy ..\build\src\Vajolet.exe ..\release\Vajolet2_3.1_bmi.exe
+copy ..\build\src\Vajolet.exe ..\release\Vajolet2_3.2_bmi.exe
 CALL clean.bat
